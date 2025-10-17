4246b176c9b47840acb2e59091e4b571cf5d1deb
MarcelaBohorquezR <ambohorquez@udistrital.edu.co>
2025-10-16 22:28:14 -0500

    Add author Maxito

--- src/BaseCalculator.jsx
+++ src/BaseCalculator.jsx
@@ -75,6 +75,7 @@ export default function BaseCalculator() {
     <div style={{ maxWidth: 500, margin: "auto", padding: 20 }}>
       <h2>🧮 Calculadora de Bases Numéricas (2–10)</h2>
       <h2>🧮 Angela Marcela Bohorquez - 20251695001</h2>
+      <h2>🧮 Maximiliano Valeta - 20251695010</h2>
 
       <div style={{ display: "grid", gap: 10 }}>
         <label>
